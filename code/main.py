47f0a323596f27c2441daec0a7630aa853755977
kb29x37 <l.coulon25480@gmail.com>
2018-12-20 14:01:53 +0100

    added overfitting plot

--- code/main.py
+++ code/main.py
@@ -8,19 +8,21 @@ import matplotlib.image as image
 
 from hyperparameters import *
 
+
+
 train_loader, test_loader = load_data.load_MNIST_dataset()
 
-model = utils.load_model(models.MAE_cleaned())
+#model = utils.load_model(models.MAE_cleaned())
 
 #load_data.show_dataset_image(train_loader)
 
 #model = models.VAE_conv_mnist()
 #model = models.MAE_conv_mnist()
-#model = models.VAE()
+model = models.VAE()
 #model = models.MAE_cleaned()
 
 #trainer.train_convnet(train_loader, model)
-#trainer.train_fully_connected(train_loader, model)
+trainer.train_fully_connected(train_loader, model, test_loader)
 
 print("done training")
 
--- latex/auto/iclr2019_conference.el
+++ latex/auto/iclr2019_conference.el
@@ -1,14 +1,12 @@
 (TeX-add-style-hook
  "iclr2019_conference"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("graphicx" "dvips" "pdftex")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -17,20 +15,14 @@
     "art10"
     "times"
     "hyperref"
-    "url"
-    "graphicx")
+    "url")
    (TeX-add-symbols
     "fix"
-    "new"
-    "arraystretch")
+    "new")
    (LaTeX-add-labels
     "loss1"
     "reparam"
-    "loss2"
-    "gen_inst"
-    "headings"
-    "others"
-    "sample-table")
+    "loss2")
    (LaTeX-add-bibliographies))
  :latex)
 
