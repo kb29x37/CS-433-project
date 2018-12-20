a8306b20f2338d75cbe415d7a3ade8c3ed4ac7d0
shellshock <olivier.lemer@epfl.ch>
2018-12-20 23:31:37 +0100

    added references to tex

--- latex/auto/iclr2019_conference.el
+++ latex/auto/iclr2019_conference.el
@@ -1,31 +1,9 @@
 (TeX-add-style-hook
  "iclr2019_conference"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
-   (TeX-run-style-hooks
-    "latex2e"
-    "math_commands"
-    "article"
-    "art10"
-    "times"
-    "hyperref"
-    "url"
-    "graphicx"
-    "subfig")
-   (TeX-add-symbols
-    "fix"
-    "new")
-   (LaTeX-add-labels
-    "loss1"
-    "reparam"
-    "loss2"
-    "fig:vae")
-   (LaTeX-add-bibliographies))
- :latex)
+   (LaTeX-add-bibitems
+    "vae"
+    "vlae"
+    "bowman"))
+ :bibtex)
 
